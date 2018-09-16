0f88c440fc4315a105752560098543c26d41186e
Nick Snyder <nick@sourcegraph.com>
2018-09-15 21:26:46 -0400

    refactor: add config

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+'use strict'
+
+import vscode from 'vscode'
+
+export function getSourcegraphUrl(): string {
+    const url = vscode.workspace.getConfiguration('sourcegraph').get<string>('url')! // has default value
+    if (url.endsWith('/')) {
+        return url.slice(0, -1)
+    }
+    return url
+}
