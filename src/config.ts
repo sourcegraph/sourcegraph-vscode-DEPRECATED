0da5171cf72fb97298870d7c965f4c84a2e520d0
Nick Snyder <nick@sourcegraph.com>
2018-09-15 21:36:55 -0400

    refactor: remove debug info

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,6 @@
 import vscode from 'vscode'
 
 export function getSourcegraphUrl(): string {
-    return 'http://localhost:3080'
     const url = vscode.workspace.getConfiguration('sourcegraph').get<string>('url')! // has default value
     if (url.endsWith('/')) {
         return url.slice(0, -1)
@@ -12,6 +11,5 @@ export function getSourcegraphUrl(): string {
 }
 
 export function getAccessToken(): string | undefined {
-    // return '5328077082b4ab52940882da16b21be10abef26a'
     return vscode.workspace.getConfiguration('sourcegraph').get<string>('accessToken')
 }
