0727f3619b22ce90abe635eb340a7744e5352a17
Alex Schneider <alexanders@twitter.com>
2021-04-05 13:02:27 -0700

    test: add failing test for branch not pushed to upstream

--- src/git/remoteNameAndBranch.test.ts
+++ src/git/remoteNameAndBranch.test.ts
@@ -61,5 +61,15 @@ describe('git', () => {
             assert.strictEqual(remoteName, 'remote-one', 'incorrect remote name')
             assert.strictEqual(branch, 'feature', 'incorrect branch name')
         })
+
+        it('falls back to first remote and branch when branch is not pushed to upstream', async () => {
+            const { remoteName, branch } = await gitRemoteNameAndBranch(
+                '',
+                createMockGitHelpers(['origin'], 'feature', 'origin/master')
+            )
+
+            assert.strictEqual(remoteName, 'origin', 'incorrect remote name')
+            assert.strictEqual(branch, 'master', 'incorrect branch name')
+        })
     })
 })
