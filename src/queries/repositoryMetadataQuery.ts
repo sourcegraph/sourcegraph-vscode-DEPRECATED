olafurpg/sourcegraph-vscode

feat: add ability to browse and search through VS Code
Previously, this extension only supported features that allowed the user
to open Sourcegraph in the browser for further exploration. This commits
adds several new features that allows the user to browse Sourcegraph
while staying in the editor:

- New Sourcegraph file system with tree view
- New Sourcegraph notebooks to run search queries

src/queries/repositoryMetadataQuery.ts
@@ -1,0 +1,76 @@
+import * as vscode from 'vscode'
+import graphqlQuery from '../queries/graphqlQuery'
+import gql from 'tagged-template-noop'
+
+export interface RepositoryMetadata {
+    defaultOid?: string
+    defaultAbbreviatedOid?: string
+    defaultBranch?: string
+    id?: string
+    commitToReferenceName?: Map<string, string>
+}
+
+export default async function repositoryMetadataQuery(
+    parameters: RevisionParameters,
+    token: vscode.CancellationToken
+): Promise<RepositoryMetadata> {
+    const response = await graphqlQuery<RevisionParameters, RevisionResult>(
+        gql`
+            query Revision($repositoryName: String!) {
+                repositoryRedirect(name: $repositoryName) {
+                    ... on Repository {
+                        id
+                        mirrorInfo {
+                            cloneInProgress
+                            cloneProgress
+                            cloned
+                        }
+                        commit(rev: "") {
+                            oid
+                            abbreviatedOID
+                            tree(path: "") {
+                                url
+                            }
+                        }
+                        defaultBranch {
+                            abbrevName
+                        }
+                    }
+                    ... on Redirect {
+                        url
+                    }
+                }
+            }
+        `,
+        parameters,
+        token
+    )
+    return {
+        id: response?.data?.repositoryRedirect?.id,
+        defaultOid: response?.data?.repositoryRedirect?.commit?.oid,
+        defaultAbbreviatedOid: response?.data?.repositoryRedirect?.commit?.abbreviatedOID,
+        defaultBranch: response?.data?.repositoryRedirect?.defaultBranch?.abbrevName,
+    }
+}
+
+interface RevisionParameters {
+    repositoryName: string
+}
+
+interface RevisionResult {
+    data?: {
+        repositoryRedirect?: {
+            id?: string
+            commit?: {
+                oid?: string
+                abbreviatedOID?: string
+                tree?: {
+                    url?: string
+                }
+            }
+            defaultBranch?: {
+                abbrevName?: string
+            }
+        }
+    }
+}

sourcegraph/sourcegraph-vscode-DEPRECATED

src/queries/repositoryMetadataQuery.ts
@@ -1,0 +1,76 @@
+import * as vscode from 'vscode'
+import { graphqlQuery } from './graphqlQuery'
+import gql from 'tagged-template-noop'
+
+export interface RepositoryMetadata {
+    defaultOid?: string
+    defaultAbbreviatedOid?: string
+    defaultBranch?: string
+    id?: string
+    commitToReferenceName?: Map<string, string>
+}
+
+export async function repositoryMetadataQuery(
+    parameters: RevisionParameters,
+    token: vscode.CancellationToken
+): Promise<RepositoryMetadata> {
+    const response = await graphqlQuery<RevisionParameters, RevisionResult>(
+        gql`
+            query RepositoryMetadata($repositoryName: String!) {
+                repositoryRedirect(name: $repositoryName) {
+                    ... on Repository {
+                        id
+                        mirrorInfo {
+                            cloneInProgress
+                            cloneProgress
+                            cloned
+                        }
+                        commit(rev: "") {
+                            oid
+                            abbreviatedOID
+                            tree(path: "") {
+                                url
+                            }
+                        }
+                        defaultBranch {
+                            abbrevName
+                        }
+                    }
+                    ... on Redirect {
+                        url
+                    }
+                }
+            }
+        `,
+        parameters,
+        token
+    )
+    return {
+        id: response?.data?.repositoryRedirect?.id,
+        defaultOid: response?.data?.repositoryRedirect?.commit?.oid,
+        defaultAbbreviatedOid: response?.data?.repositoryRedirect?.commit?.abbreviatedOID,
+        defaultBranch: response?.data?.repositoryRedirect?.defaultBranch?.abbrevName,
+    }
+}
+
+interface RevisionParameters {
+    repositoryName: string
+}
+
+interface RevisionResult {
+    data?: {
+        repositoryRedirect?: {
+            id?: string
+            commit?: {
+                oid?: string
+                abbreviatedOID?: string
+                tree?: {
+                    url?: string
+                }
+            }
+            defaultBranch?: {
+                abbrevName?: string
+            }
+        }
+    }
+}